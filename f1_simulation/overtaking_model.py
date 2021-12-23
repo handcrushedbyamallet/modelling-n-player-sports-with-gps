83374c3b6de56069bc508464449d46a7376d668f
Euodia Dodd <euodiadodd@yahoo.com>
2021-12-23 17:50:26 +0000

    Cleaned up the code and added input params

--- f1_simulation/overtaking_model.py
+++ f1_simulation/overtaking_model.py
@@ -12,56 +12,50 @@ GPy.plotting.change_plotting_library('matplotlib')
 data = F1Dataset('data')
 
 def make_overtaking_process(driver: str, constructor: str, course: str, year: int):
-    #join_df = data.lap_times.join(data.pit_stops.set_index(["raceId", "driverId", "lap"]).reset_index(), rsuffix="pitstop")
-    #data.lap_times["didPitStop"] = np.logical_not(data.lap_times.join(data.pit_stops.set_index(["raceId", "driverId", "lap"]).reset_index(), rsuffix="pitstop")["stops"].isna())
-
-    ##TODO: Remove laps with pit stops
+    
     lp = data.lap_times
+    ##Filter out laps with pitstops
+    lp["didPitStop"] = np.logical_not(lp.join(data.pit_stops.set_index(["raceId", "driverId", "lap"]), on=['raceId', 'driverId', 'lap'], rsuffix='pit_stops')["stop"].isna())
+    lp = lp.loc[lp["didPitStop"] == False]
 
-    races = lp.groupby(["raceId"])
-
-    overtaking = pd.DataFrame()
-
-    for race, df_race in races:
-        for driverId in list(set(df_race['driverId'])):
-            driverRef = get_driver_ref(driverId)
-            position_diff = df_race.loc[df_race['driverId'] == driverId]['position'].diff()
-            pos_change = sum(x for x in position_diff if x > 0)
-            neg_change = sum(x for x in position_diff if x < 0)
-            #print(pos_change)
-            avg_laptime = df_race.loc[df_race['driverId'] == driverId]['milliseconds'].mean()
-            new_df = pd.DataFrame({'raceId': [race],'driver': [driverId], 'overtakes':[pos_change], 'overtaken':[neg_change], 'avg_laptime': [avg_laptime]})
-            overtaking = pd.concat([overtaking, new_df])
+    lp = lp.join(data.races.set_index(['raceId'])[['year']], on=['raceId'])
+    lp = lp.join(data.results.set_index(['raceId', 'driverId'])[['constructorId']], on=['raceId', 'driverId'])
+    
+            
+    lp['num_overtakes'] = lp.groupby(['raceId', 'driverId'])['position'].diff()
+    lp['avg_laptime'] = lp.groupby(['raceId', 'driverId'])['milliseconds'].transform(np.mean)
+    lp['overtakes'] = lp.groupby(['raceId', 'driverId'])['num_overtakes'].transform(lambda x: x[x > 0].sum())
+    overtaking = lp.groupby(['raceId', 'driverId']).first().reset_index()
 
     
-    # lp['num_overtakes'] = lp.groupby(['raceId', 'driverId'])['position'].diff()
-    # lp['average_lap_time'] = lp.groupby(['raceId', 'driverId'])['milliseconds'].transform(np.mean)
-    # lp['overtakes'] = lp.groupby(['raceId', 'driverId']).transform(lambda x: sum(y for y in x if y > 0))
-    # overtakes = lp.groupby(['raceId', 'driverId']).mean()
+    overtaking = overtaking.loc[overtaking['raceId'] == 841]
     
-    #overtaking = overtaking.loc[overtaking['raceId'] == 841]
-    ##TODO: replace overtaken with starting position, qualifying time etc etc
-    X = overtaking[['avg_laptime', 'raceId', 'driver']]
+    X = overtaking[['avg_laptime','driverId', 'year', 'constructorId']]
     Y = overtaking[['overtakes']]
     
  
-    kernel = GPy.kern.RBF(input_dim=3, lengthscale=500)
+    kernel = GPy.kern.RBF(input_dim=4, lengthscale=500)
     m = GPy.models.GPRegression(X,Y,kernel)
-    # m.optimize(messages=True)
-    # m.optimize_restarts(num_restarts = 10)
-    print(m.predict([[10000], [841],[1]]))
-    # m.plot()
-    # plt.show(block=True) 
+    m.optimize(messages=True)
+    m.optimize_restarts(num_restarts = 10)
+    # #print(m.predict([[10000], [841],[1]]))
+    # # m.plot()
+    # # plt.show(block=True) 
 
-    ## Taking slices to condition on second input param
-    #slices = 841]
-    #for i, y in zip(range(3), slices):
-        # m.optimize(messages=True)
-        # m.optimize_restarts(num_restarts = 10)
+    # ## Taking slices to condition on second input param
+    # slices = [841]
+    # for i, y in zip(range(3), slices):
+    #     m.optimize(messages=True)
+    #     m.optimize_restarts(num_restarts = 10)
     
-    ##Set course to 841 and driver to Hamilton
-    #m.plot(fixed_inputs=[(1,841), (2,1)], plot_data=True)
-    #plt.show(block=True) 
+    ## Set course to 841 and driver to Hamilton
+    m.plot(fixed_inputs=[(1,841), (2,2021), (3,1)], plot_data=True)
+    plt.show(block=True) 
+
+    ##TODO: Either change input params to take ID, or write helper methods to converst strings to IDs
+
+    result = m.predict([[driver], [constructor], [course],[year]])
+    print(result)
 
     return lambda opponent: random.random() > 0.5
 
